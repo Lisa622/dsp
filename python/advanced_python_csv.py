c984b0e6ff9bb845b2ff3c3251c79b63817574f0
Lisa622 <angryasiangrrl@gmail.com>
2017-01-02 02:33:36 -0500

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,16 @@
-PLACE YOUR CODE HERE
+import csv
+import urllib.request
+
+url = 'https://raw.githubusercontent.com/Lisa622/dsp/master/python/faculty.csv'
+webpage = urllib.request.urlopen(url)
+webpage_read = webpage.read().decode('utf-8').splitlines()
+datareader = csv.reader(webpage_read)
+data = []
+for row in datareader:
+    data.append(row)
+data.pop(0)
+emails = [x[3] for x in data]
+outputfile = open('emails.csv', 'w', newline ='')
+outputwriter = csv.writer(outputfile)
+for i in emails:
+    outputwriter.writerow([i])
