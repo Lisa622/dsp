e6baf85796fc523efb57d11af029fe767b7f8eaf
Lisa622 <angryasiangrrl@gmail.com>
2017-01-02 01:56:27 -0500

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,23 @@
-PLACE YOUR CODE HERE
+import csv
+import urllib.request
+import re
+
+url = 'https://raw.githubusercontent.com/Lisa622/dsp/master/python/faculty.csv'
+webpage = urllib.request.urlopen(url)
+webpage_read = webpage.read().decode('utf-8').splitlines()
+datareader = csv.reader(webpage_read)
+data = []
+for row in datareader:
+    data.append(row)
+for row in data:
+    print(row)
+data.pop(0)
+emails = [x[3] for x in data]
+#for i in emails:
+    #print (i)
+domains = []
+for i in emails:
+    domain = re.search("@[\w.]+", i)
+    domains.append(domain.group())
+uniquedomains = set(domains)
+print(uniquedomains)
